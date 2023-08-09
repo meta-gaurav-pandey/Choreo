7cf7232622a763ffc956576e6785df24de49caa0
Gaurav Pandey <gaurav.pandey@metacube.com>
2023-08-09 15:01:07 +0530

    changes for database

--- JavaClassMediator/src/main/java/com/example/JavaMediatorClass.java
+++ JavaClassMediator/src/main/java/com/example/JavaMediatorClass.java
@@ -1,159 +1,327 @@
 package com.example;
 
-import org.apache.synapse.MessageContext; 
+import org.apache.synapse.MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
 
-public class JavaMediatorClass extends AbstractMediator { 
- 
-	public boolean mediate(MessageContext context) { 
-		// TODO Implement your mediation logic here 
-	   
-		   JSONArray json;
-         String payload = context.getEnvelope().getBody().toString();
-      
-       String ObjectType=(String)context.getProperty("sObjectType");
-       System.out.println(ObjectType);
-         try {
-        	 if(ObjectType.equals("Account")) {
-        		 System.out.println("Account"+payload);
-			json=convertAccount(payload);
-			context.setProperty("Array", json.toString());
+public class JavaMediatorClass extends AbstractMediator {
+
+	public boolean mediate(MessageContext context) {
+		// TODO Implement your mediation logic here
+
+		JSONArray json;
+		String payload = context.getEnvelope().getBody().toString();
+
+		String ObjectType = (String) context.getProperty("sObjectType");
+		System.out.println(ObjectType);
+		try {
+			if (ObjectType.equals("Account")) {
+				System.out.println("Account" + payload);
+				json = convertAccount(payload);
+				context.setProperty("Array", json.toString());
+			} else if (ObjectType.equals("Lead")) {
+				System.out.println("Lead" + payload);
+				json = convertLead(payload);
+				context.setProperty("Array", json.toString());
+			} else if (ObjectType.equals("AccountScheduler")) {
+				System.out.println("Scheduler" + payload);
+				json = convertSchedule(payload);
+				context.setProperty("Array", json.toString());
+			} else if (ObjectType.equals("DataEvent")) {
+				System.out.println("DataEvent" + payload);
+				json = convertEventData(payload);
+				context.setProperty("Array", json.toString());
+			} else if (ObjectType.equals("ApiData")) {
+				System.out.println("ApiData" + payload);
+				json = convertApiData(payload);
+				context.setProperty("Array", json.toString());
+			} else {
+				throw new Exception("The typeObject is null");
 			}
-        	 else if(ObjectType.equals("Lead")) {
-        		 System.out.println("Lead"+payload);
-        		 json=convertLead(payload);
-     			context.setProperty("Array", json.toString()); 
-        	 }
-        	 else if (ObjectType.equals("AccountScheduler")) {
-        		 System.out.println("Scheduler"+payload);
-        		 json=convert(payload);
-      			context.setProperty("Array", json.toString()); 
-        	 }
-        	 else {
-        		 throw new Exception("The typeObject is null");
-        	 }
-        	 
-			 } catch (Exception e) {
-				System.out.println("Error occure while converting into json object either records are empty or retrival failed");
-				
-			}
-         
-     return true;
- }
-	
-	
+
+		} catch (Exception e) {
+			System.out.println(
+					"Error occure while converting into json object either records are empty or retrival failed");
+
+		}
+
+		return true;
+	}
+
 	public static JSONArray convertAccount(String xml) throws org.json.JSONException {
-		
-  if(xml.length()>0) {
- 	 JSONObject jsonObj = XML.toJSONObject(xml);
-   //int count=jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getInt("totalSize");
-  JSONArray records;
- // if(count==1) {
-	  records =new  JSONArray();
-	   records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONObject("sobject"));
 
-//  }
-//  else {
-  // records = jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONArray("records");
- 
- // }
-  System.out.println("Hey" + records);
-  JSONArray result = new JSONArray();
-  for (int i = 0; i < records.length(); i++) {
- 	 
-      JSONObject record = records.getJSONObject(i);
-      JSONObject obj = new JSONObject();
-      obj.put("name", record.getString("Name"));
-      obj.put("Id", record.getString("Id"));
-    //  obj.put("type", record.getJSONObject("attributes").getString("type"));
-    //  obj.put("AccountNo", record.getString("AccountNumber"));
-    //  obj.put("description", record.getString("Description"));
-      obj.put("type", "Account");
-      result.put(obj);
-      
-  }
-  return result;
-	}
-else { JSONArray result = new JSONArray();
-	return result;
-}
+		if (xml.length() > 0) {
+			JSONObject jsonObj = XML.toJSONObject(xml);
 
-	}
-	public static JSONArray convertLead(String xml) throws org.json.JSONException {
-		
-		  if(xml.length()>0) {
-		 	 JSONObject jsonObj = XML.toJSONObject(xml);
-		   //int count=jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getInt("totalSize");
-		  JSONArray records;
-		 // if(count==1) {
-			  records =new  JSONArray();
-			   records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONObject("records"));
+			JSONArray records;
 
-		//  }
-		//  else {
-		  // records = jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONArray("records");
-		 
-		 // }
-		  System.out.println("Hey" + records);
-		  JSONArray result = new JSONArray();
-		  for (int i = 0; i < records.length(); i++) {
-		 	 
-		      JSONObject record = records.getJSONObject(i);
-		      JSONObject obj = new JSONObject();
-		      obj.put("name", record.getString("Company"));
-		      //obj.put("Id", record.getString("Id"));
-		    //  obj.put("type", record.getJSONObject("attributes").getString("type"));
-		        obj.put("country", record.getString("Country"));
-		        obj.put("description", record.getString("Description"));
-		        obj.put("source", "salesforce");
-		      
-		      result.put(obj);
-		      
-		  }
-		  return result;
+			records = new JSONArray();
+			records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONObject("sobject"));
+
+			System.out.println("Hey" + records);
+			JSONArray result = new JSONArray();
+			for (int i = 0; i < records.length(); i++) {
+
+				JSONObject record = records.getJSONObject(i);
+				JSONObject obj = new JSONObject();
+				obj.put("name", record.getString("Name"));
+				obj.put("Id", record.getString("Id"));
+				// obj.put("type", record.getJSONObject("attributes").getString("type"));
+				// obj.put("AccountNo", record.getString("AccountNumber"));
+				// obj.put("description", record.getString("Description"));
+				obj.put("type", "Account");
+				result.put(obj);
+
 			}
-		else { JSONArray result = new JSONArray();
+			return result;
+		} else {
+			JSONArray result = new JSONArray();
 			return result;
 		}
 
+	}
+
+	public static JSONArray convertLead(String xml) throws org.json.JSONException {
+
+		if (xml.length() > 0) {
+			JSONObject jsonObj = XML.toJSONObject(xml);
+
+			JSONArray records;
+
+			records = new JSONArray();
+			records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONObject("records"));
+
+			// System.out.println("Hey" + records);
+			JSONArray result = new JSONArray();
+			for (int i = 0; i < records.length(); i++) {
+
+				JSONObject record = records.getJSONObject(i);
+				JSONObject obj = new JSONObject();
+				obj.put("name", record.get("Company"));
+				obj.put("description", record.get("Description"));
+				obj.put("source", "salesforce");
+				obj.put("isDeleted", record.get("IsDeleted"));
+
+				obj.put("masterRecordId", record.get("MasterRecordId"));
+				obj.put("lastName", record.get("LastName"));
+				obj.put("firstName", record.get("FirstName"));
+				obj.put("title", record.get("Title"));
+				// obj.put("company", record.get("Company"));
+				obj.put("street", record.get("Street"));
+				obj.put("city", record.get("City"));
+				obj.put("state", record.get("State"));
+				obj.put("postalCode", record.get("PostalCode"));
+				obj.put("country", record.get("Country"));
+				obj.put("latitude", record.get("Latitude"));
+				obj.put("longitude", record.get("Longitude"));
+				obj.put("geocodeAccuracy", record.get("GeocodeAccuracy"));
+				obj.put("phone", record.get("Phone"));
+				obj.put("mobilePhone", record.get("MobilePhone"));
+				obj.put("fax", record.get("Fax"));
+				obj.put("email", record.get("Email"));
+				obj.put("website", record.get("Website"));
+				obj.put("status", record.get("Status"));
+				obj.put("industry", record.get("Industry"));
+				obj.put("rating", record.get("Rating"));
+				obj.put("annualRevenue", record.get("AnnualRevenue"));
+				obj.put("no_of_employees", record.get("NumberOfEmployees"));
+				obj.put("SIC", record.get("SICCode__c"));
+				obj.put("type", record.get("Company_type__c"));
+				obj.put("annual_growth", record.get("Annual_Growth__c"));
+				obj.put("NAICS", record.get("NAICS__c"));
+				obj.put("second_last_quarterly_revenue", record.get("second_last_quarterly_revenue__c"));
+				obj.put("last_quarterly_revenue", record.get("SICCode__c"));
+				obj.put("last_quarterly_revenue", record.get("last_quarterly_revenue__c"));
+				obj.put("yearly_revenue_2021", record.get("yearly_revenue_2021__c"));
+				obj.put("yearly_revenue_2022", record.get("yearly_revenue_2022__c"));
+				obj.put("Categories", record.get("Categories__c"));
+				obj.put("Specialties", record.get("Specialties__c"));
+				obj.put("headquarters", record.get("headquarters__c"));
+				result.put(obj);
+
 			}
-	public static JSONArray convert(String xml) throws org.json.JSONException {
-		
-	if(xml.length()>0) {
-        JSONObject jsonObj = XML.toJSONObject(xml);
-    int count=jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getInt("totalSize");
-    JSONArray records;
-    if(count==1) {
-         records =new  JSONArray();
-         records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONObject("records"));
+			return result;
+		} else {
+			JSONArray result = new JSONArray();
+			return result;
+		}
 
-    }
-    else {
-     records = jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONArray("records");
-   
-    }
-    System.out.println("Hey" + records);
-    JSONArray result = new JSONArray();
-    for (int i = 0; i < records.length(); i++) {
-        
-        JSONObject record = records.getJSONObject(i);
-        JSONObject obj = new JSONObject();
-        obj.put("name", record.getString("Name"));
-        obj.put("Id", record.getString("Id"));
-        obj.put("type", record.getJSONObject("attributes").getString("type"));
-      //  obj.put("AccountNo", record.getString("AccountNumber"));
-      //  obj.put("description", record.getString("Description"));
-        result.put(obj);
-        
-    }
-    return result;
-      }
-  else { JSONArray result = new JSONArray();
-      return result;
-  }
+	}
 
-}
+	public static JSONArray convertSchedule(String xml) throws org.json.JSONException {
+
+		if (xml.length() > 0) {
+			JSONObject jsonObj = XML.toJSONObject(xml);
+			int count = jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getInt("totalSize");
+			JSONArray records;
+			if (count == 1) {
+				records = new JSONArray();
+				records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONObject("records"));
+
+			} else {
+				records = jsonObj.getJSONObject("soapenv:Body").getJSONObject("jsonObject").getJSONArray("records");
+
+			}
+			System.out.println("Hey" + records);
+			JSONArray result = new JSONArray();
+			for (int i = 0; i < records.length(); i++) {
+
+				JSONObject record = records.getJSONObject(i);
+				JSONObject obj = new JSONObject();
+				obj.put("name", record.getString("Name"));
+				obj.put("Id", record.getString("Id"));
+				obj.put("type", record.getJSONObject("attributes").getString("type"));
+				result.put(obj);
+
+			}
+			return result;
+		} else {
+			JSONArray result = new JSONArray();
+			return result;
+		}
+
+	}
+
+	public static JSONArray convertEventData(String xml) throws org.json.JSONException {
+
+		if (xml.length() > 0) {
+			JSONObject jsonObj = XML.toJSONObject(xml);
+		//	System.out.println(jsonObj);
+			JSONArray records;
+
+			records = new JSONArray();
+			records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("Record"));
+
+
+			JSONArray result = new JSONArray();
+			for (int i = 0; i < records.length(); i++) {
+
+				JSONObject record = records.getJSONObject(i);
+				JSONObject obj = new JSONObject();
+				obj.put("name", record.get("name") instanceof JSONObject ? "" : record.get("name"));
+
+				obj.put("website", record.get("website") instanceof JSONObject ? "" : record.get("website"));
+				obj.put("source", record.get("source") instanceof JSONObject ? "" : record.get("source"));
+				obj.put("industry", record.get("industry") instanceof JSONObject ? "" : record.get("industry"));
+
+				obj.put("headquarters",
+						record.get("headquarters") instanceof JSONObject ? "" : record.get("headquarters"));
+				obj.put("country", record.get("country") instanceof JSONObject ? "" : record.get("country"));
+				obj.put("market_value",
+						record.get("market_value") instanceof JSONObject ? "" : record.get("market_value"));
+				obj.put("annual_growth",
+						record.get("annual_growth") instanceof JSONObject ? "" : record.get("annual_growth"));
+				// obj.put("company", record.get("Company"));
+				obj.put("quarterly_growth",
+						record.get("quarterly_growth") instanceof JSONObject ? "" : record.get("quarterly_growth"));
+				obj.put("no_of_employees",
+						record.get("no_of_employees") instanceof JSONObject ? "" : record.get("no_of_employees"));
+				obj.put("type", record.get("type") instanceof JSONObject ? "" : record.get("type"));
+				obj.put("founded", record.get("founded") instanceof JSONObject ? "" : record.get("founded"));
+				obj.put("assets", record.get("assets") instanceof JSONObject ? "" : record.get("assets"));
+				obj.put("sales", record.get("sales") instanceof JSONObject ? "" : record.get("sales"));
+				obj.put("profit", record.get("profit") instanceof JSONObject ? "" : record.get("profit"));
+				obj.put("yearly_revenue_2022", record.get("yearly_revenue_2022") instanceof JSONObject ? ""
+						: record.get("yearly_revenue_2022"));
+				obj.put("yearly_revenue_2021", record.get("yearly_revenue_2021") instanceof JSONObject ? ""
+						: record.get("yearly_revenue_2021"));
+				obj.put("last_quarterly_revenue", record.get("last_quarterly_revenue") instanceof JSONObject ? ""
+						: record.get("last_quarterly_revenue"));
+				obj.put("second_last_quarterly_revenue",
+						record.get("second_last_quarterly_revenue") instanceof JSONObject ? ""
+								: record.get("second_last_quarterly_revenue"));
+				obj.put("article", record.get("article") instanceof JSONObject ? "" : record.get("article"));
+				obj.put("SIC", record.get("SIC") instanceof JSONObject ? "" : record.get("SIC"));
+				obj.put("NAICS", record.get("NAICS") instanceof JSONObject ? "" : record.get("NAICS"));
+				obj.put("company_size",
+						record.get("company_size") instanceof JSONObject ? "" : record.get("company_size"));
+				obj.put("Specialties",
+						record.get("Specialties") instanceof JSONObject ? "" : record.get("Specialties"));
+
+				result.put(obj);
+
+			}
+			return result;
+		} else {
+			JSONArray result = new JSONArray();
+			return result;
+		}
+
+	}
+
+	public static JSONArray convertApiData(String xml) throws org.json.JSONException {
+
+		if (xml.length() > 0) {
+			JSONObject jsonObj = XML.toJSONObject(xml);
+			// int
+			// count=jsonObj.getJSONJSONObject("soapenv:Body").getJSONJSONObject("jsonJSONObject").getInt("totalSize");
+			JSONArray records;
+
+			records = new JSONArray();
+			if (jsonObj.getJSONObject("soapenv:Body").getJSONObject("SourceData").get("Data") instanceof JSONObject) {
+				records.put(jsonObj.getJSONObject("soapenv:Body").getJSONObject("SourceData").get("Data"));
+			} else if (jsonObj.getJSONObject("soapenv:Body").getJSONObject("SourceData")
+					.get("Data") instanceof JSONArray) {
+				records = jsonObj.getJSONObject("soapenv:Body").getJSONObject("SourceData").getJSONArray("Data");
+			} else {
+				System.out.println("No data is comming of error occured");
+			}
+
+			JSONArray result = new JSONArray();
+			for (int i = 0; i < records.length(); i++) {
+
+				JSONObject record = records.getJSONObject(i);
+				JSONObject obj = new JSONObject();
+				obj.put("name", record.get("name") instanceof JSONObject ? "" : record.get("name"));
+
+				obj.put("website", record.get("website") instanceof JSONObject ? "" : record.get("website"));
+				obj.put("source", record.get("source") instanceof JSONObject ? "" : record.get("source"));
+				obj.put("industry", record.get("industry") instanceof JSONObject ? "" : record.get("industry"));
+
+				obj.put("headquarters",
+						record.get("headquarters") instanceof JSONObject ? "" : record.get("headquarters"));
+				obj.put("country", record.get("country") instanceof JSONObject ? "" : record.get("country"));
+				obj.put("market_value",
+						record.get("market_value") instanceof JSONObject ? "" : record.get("market_value"));
+				obj.put("annual_growth",
+						record.get("annual_growth") instanceof JSONObject ? "" : record.get("annual_growth"));
+				// obj.put("company", record.get("Company"));
+				obj.put("quarterly_growth",
+						record.get("quarterly_growth") instanceof JSONObject ? "" : record.get("quarterly_growth"));
+				obj.put("no_of_employees",
+						record.get("no_of_employees") instanceof JSONObject ? "" : record.get("no_of_employees"));
+				obj.put("type", record.get("type") instanceof JSONObject ? "" : record.get("type"));
+				obj.put("founded", record.get("founded") instanceof JSONObject ? "" : record.get("founded"));
+				obj.put("assets", record.get("assets") instanceof JSONObject ? "" : record.get("assets"));
+				obj.put("sales", record.get("sales") instanceof JSONObject ? "" : record.get("sales"));
+				obj.put("profit", record.get("profit") instanceof JSONObject ? "" : record.get("profit"));
+				obj.put("yearly_revenue_2022", record.get("yearly_revenue_2022") instanceof JSONObject ? ""
+						: record.get("yearly_revenue_2022"));
+				obj.put("yearly_revenue_2021", record.get("yearly_revenue_2021") instanceof JSONObject ? ""
+						: record.get("yearly_revenue_2021"));
+				obj.put("last_quarterly_revenue", record.get("last_quarterly_revenue") instanceof JSONObject ? ""
+						: record.get("last_quarterly_revenue"));
+				obj.put("second_last_quarterly_revenue",
+						record.get("second_last_quarterly_revenue") instanceof JSONObject ? ""
+								: record.get("second_last_quarterly_revenue"));
+				obj.put("article", record.get("article") instanceof JSONObject ? "" : record.get("article"));
+				obj.put("SIC", record.get("SIC") instanceof JSONObject ? "" : record.get("SIC"));
+				obj.put("NAICS", record.get("NAICS") instanceof JSONObject ? "" : record.get("NAICS"));
+				obj.put("company_size",
+						record.get("company_size") instanceof JSONObject ? "" : record.get("company_size"));
+				obj.put("Specialties",
+						record.get("Specialties") instanceof JSONObject ? "" : record.get("Specialties"));
+
+				result.put(obj);
+
+			}
+			return result;
+		} else {
+			JSONArray result = new JSONArray();
+			return result;
+		}
+
+	}
 }
